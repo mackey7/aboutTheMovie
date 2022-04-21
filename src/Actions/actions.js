cd0931c3cc692888c25d551d23c3a69e1fae58ca
Maciej Bogdański <m.bogdanskii@gmail.com>
2022-04-21 19:12:45 +0200

    create import movie serach from api action and case in reducers

--- src/Actions/actions.js
+++ src/Actions/actions.js
@@ -1,4 +1,27 @@
-import { FETCH_MOVIE_ITEM } from "./types";
+import axios from "axios";
+import { FETCH_MOVIE_ITEM, FETCH_SERACH_MOVIE } from "./types";
+import { API_KEY } from "../env/key";
+
+export const ImportMovieSerachFromApi = (query) => {
+  return (dispatch) => {
+    return axios
+      .get(
+        `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${query}&page=1&include_adult=false`
+      )
+      .then((response) => {
+        dispatch(fetchSerchedMoviesSuccess(response.data));
+      })
+      .catch((error) => {
+        throw error;
+      });
+  };
+};
+export const fetchSerchedMoviesSuccess = (payload) => {
+  return {
+    type: FETCH_SERACH_MOVIE,
+    payload,
+  };
+};
 
 export const fetchMoviesSuccess = (payload) => {
   return {
--- src/Reducers/movieReducer.js
+++ src/Reducers/movieReducer.js
@@ -1,9 +1,15 @@
-import { FETCH_MOVIE_ITEM } from "../Actions/types";
-const initial = {};
+import { FETCH_MOVIE_ITEM, SERACH_MOVIE } from "../Actions/types";
+const initial = {
+  serched_movies: [],
+  movie_item: [],
+};
 const moviesReducer = (state = null, actions) => {
   switch (actions.type) {
     case FETCH_MOVIE_ITEM: {
       return { ...state, movie_item: actions.payload };
+    }
+    case FETCH_SERACH_MOVIE: {
+      return { ...state, serched_movies: actions.payload };
     }
     default:
       return state;
